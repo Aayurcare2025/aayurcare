cd9aab9fa6dcd794d0c9c6832249d0ca786ead1b
Aayurcare2025 <info@aayurcare.com>
2025-09-17 09:46:43 +0530

    push code

--- src/Dashboard/Home.js
+++ src/Dashboard/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "../App.css";
-import Family from "../images/1.png";
+// import Family from "../images/1.png";
 
 function Home() {
   return (
     <div className="main-page">
       <div className="image-container">
-        <img src={Family} alt="Family" />
+        {/* <img src={Family} alt="Family" /> */}
+        <img src="/images/1.png" alt="Family" />
+
         <div className="image-text">
           <h2>This site is temporarily down for maintenance.</h2>
           <p>Thanks for bearing with us.</p>
