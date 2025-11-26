9d2112358c68f1fc9a65250811a8fc9f25eb61be
Aayurcare2025 <nambiarkirthana23@gmail.com>
2025-11-26 09:45:30 +0530

    push

--- src/Dashboard/Home.js
+++ src/Dashboard/Home.js
@@ -51,8 +51,8 @@ function Home() {
         <div className="hero-content">
           <div className="hero-text">
                <h1 className="hero-title">
-              Smart Medical Reimbursement  <br />
-              <span className="highlight">Plans </span>
+              Smart Plan Medical Reimbursement  <br />
+              <span className="highlight">OPD Plans </span>
               <br />
               For everyone
             </h1>
